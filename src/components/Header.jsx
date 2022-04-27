755a402e65c47f81c4797bf50e7d3d1f63b014cf
WENKAITAN <wenkaitan1019@gmail.com>
2022-04-27 16:39:35 -0400

    button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -18,7 +18,7 @@ function Header() {
 
             {isAuthenticated && user  ? (
                 <>  
-                    <Link to="/studentcenter">student center</Link>
+                    <Button variant="secondary" size="lg"><Link to="/studentcenter" style={{textDecoration: 'none'}}>student center</Link></Button>
                     <Button variant="secondary" size="lg" onClick={()=> navigate('/Profile')}><MdAccountBox/></Button>
                     <LogoutButton/>
                 </>
